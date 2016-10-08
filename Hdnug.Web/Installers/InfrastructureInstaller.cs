144137daeac00a6f69e284a2684f979334ea2ef0
Jared Lobberecht <jared@lobberecht.com>
2016-10-08 15:17:32 -0500

    adding initial mailing list form

---
+++ Hdnug.Web/Installers/InfrastructureInstaller.cs
@@ -0,0 +1,22 @@
+﻿using Castle.MicroKernel.Registration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using Hdnug.Web.Inrastructure.Interfaces;
+using Hdnug.Web.Inrastructure.Providers;
+
+namespace Hdnug.Web.Installers
+{
+    public class InfrastructureInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(Component.For<IMailingListService>()
+                .ImplementedBy<MailgunMailingListService>());
+        }
+    }
+}
